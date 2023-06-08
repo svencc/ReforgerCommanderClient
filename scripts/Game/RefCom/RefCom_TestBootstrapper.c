dcf8c65c468bbb6a434a851ea226009d3219a31c
Sven Carrillo Castillo <carrillo.castillo.sven@googlemail.com>
2023-06-08 18:58:07 +0200

    prepare client development; add todo to README.md

--- scripts/Game/RefCom/RefCom_TestBootstrapper.c
+++ scripts/Game/RefCom/RefCom_TestBootstrapper.c
@@ -14,7 +14,8 @@ class RefCom_TestBootstrapper: SCR_BaseTriggerEntity
 	override void EOnInit(IEntity owner) {
 		super.EOnInit(owner);
 		PrintFormat("%1 initialized", "RefCom_TestBootstrapper");
-		if (false && GetGame().InPlayMode()) {
+		if (false) {
+		// if (GetGame().InPlayMode()) {
 			PrintFormat("%1 run %2", "RefCom_TestBootstrapper", "MapScanner");
 			mapScanner = new RefCom_MapScanner(new RefCom_MapScannerEntitiesShippingService(500), 150);
 					
