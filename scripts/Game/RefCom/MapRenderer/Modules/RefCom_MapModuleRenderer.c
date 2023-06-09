5c98a0b9653ee58eca0d6bb79eda0d093eb017ae
Sven Carrillo Castillo <carrillo.castillo.sven@googlemail.com>
2023-06-09 20:13:21 +0200

    rewrite map polygon rendering ...

--- scripts/Game/RefCom/MapRenderer/Modules/RefCom_MapModuleRenderer.c
+++ scripts/Game/RefCom/MapRenderer/Modules/RefCom_MapModuleRenderer.c
@@ -34,15 +34,21 @@ class RefCom_MapModuleRenderer  : SCR_MapModuleBase {
 		renderCircle("1000 0 1000", 50, ARGB(255, 255, 0, 0) );
 		renderCircle("1100 0 1100", 100, ARGB(255, 0, 255, 0) );
 		renderCircle("1200 0 1200", 75, ARGB(255, 0, 0, 255) );
+		renderCircle("2000 0 2000", 400, ARGB(255, 0, 0, 255));
+		renderCircleOutlined("2000 0 3000", 400, ARGB(255, 0, 0, 255), ARGB(255, 0, 255, 0), 4);
 		
-		renderCircleOutlined("2000 0 2000", 400, ARGB(255, 0, 0, 255), ARGB(255, 0, 255, 0), 4);
+		renderLineCircled("3000 0 3000", 400, ARGB(255, 0, 0, 255), 20, 16);
+		renderLineCircledOutlined("4000 0 3000", 400, ARGB(255, 0, 0, 255), 20, ARGB(255, 0, 255, 0), 4, 8);
 		
 		renderLine("800 0 800", "1500 0 1500", ARGB(255, 0, 0, 0), 2);
-		renderLineOutlined("1200 0 800", "1900 0 1500", ARGB(255, 0, 255, 0), 10, ARGB(255, 0, 0, 0), 2);
+		renderLineOutlined("1200 0 800", "1900 0 1500", ARGB(255, 0, 0, 0), 20, ARGB(255, 0, 255, 0), 2);
 		
-		//renderRectangle("2000 0 1000", "3000 0 2000", ARGB(255, 0, 0, 255));
-		renderRectangleOutlined("3150 0 1150", "3850 0 1850", ARGB(255, 0, 0, 255),  ARGB(255, 255, 0, 0), 2);
+		renderRectangle("2000 0 1000", "3000 0 2000", ARGB(255, 0, 0, 255));
+		renderRectangleOutlined("3150 0 1150", "3850 0 1850", ARGB(255, 0, 0, 255),  ARGB(255, 255, 0, 0), 2);	
 		
+		renderLineRectangled("450 0 450", "650 0 650", ARGB(255, 0, 0, 255), 2);
+		renderLineRectangledOutlined("150 0 1150", "850 0 850", ARGB(255, 0, 0, 255), 10, ARGB(255, 255, 0, 0), 2);		
+
 		//renderText("800 0 1000", "Das ist mein Text", ARGB(255, 0, 0, 0), 50);
 	}
 	
@@ -53,81 +59,179 @@ class RefCom_MapModuleRenderer  : SCR_MapModuleBase {
 		Render()
 	}
 	
-	array<vector> renderCircle(vector center, float radius, int color, int resolution = 36) {
+	
+
+	// LINE
+	void renderLine(vector start, vector end, int color, float width) {
+		array<vector> points = new array<vector>;
+		points.Insert(start);
+		points.Insert(end);
+		array<vector> translatedPoints = translatePoints(points);
+		renderRawLine(translatedPoints[0], translatedPoints[1], color, width);
+	}
+	void renderLineOutlined(vector start, vector end, int color, float width, int outlineColor,  float outlineWidth) {
+		array<vector> points = new array<vector>;
+		points.Insert(start);
+		points.Insert(end);
+		array<vector> translatedPoints = translatePoints(points);
+		renderRawLineOutlined(translatedPoints[0], translatedPoints[1], color, width, outlineColor, outlineWidth);
+	}
+	
+	
+	
+	// RECTANGLE
+	void renderRectangle(vector start, vector end, int color) {
+		array<vector> points = generateRectangularPolygon(start, end);
+		array<vector> translatedPoints = translatePoints(points);
+		renderPolygon(translatedPoints, color);
+	}
+	void renderRectangleOutlined(vector start, vector end, int color, int outlineColor,  float outlineWidth) {
+		array<vector> points = generateRectangularPolygon(start, end);
+		array<vector> translatedPoints = translatePoints(points);
+		renderPolygon(translatedPoints, color);
+		renderPolygonToLines(translatedPoints, outlineColor, outlineWidth);
+	}
+	array<vector> generateRectangularPolygon(vector start, vector end) {
+		array<vector> rectangularPoints = new array<vector>;
+		rectangularPoints.Insert(start);
+		
+		vector second = {end[0],0,start[2]};
+		rectangularPoints.Insert(second);
+		
+		rectangularPoints.Insert(end);
+		
+		vector last = {start[0],0,end[2]};
+		rectangularPoints.Insert(last);
+		
+		return rectangularPoints;
+	}
+	
+	
+	// CIRCLE
+	void renderCircleOutlined(vector center, float radius, int color, int outlineColor,  float outlineWidth, int resolution = 36) {
+		array<vector> points = generateCircularPolygon(center, radius, resolution);
+		array<vector> translatedPoints = translatePoints(points);
+		renderPolygon(translatedPoints, color);
+		renderPolygonToLines(translatedPoints, outlineColor, outlineWidth);
+	}
+	void renderCircle(vector center, float radius, int color, int resolution = 36) {
+		array<vector> points = generateCircularPolygon(center, radius, resolution);
+		array<vector> translatedPoints = translatePoints(points);
+		renderPolygon(translatedPoints, color);
+	}
+	array<vector> generateCircularPolygon(vector center, float radius, int resolution = 36) {
+		array<vector> circularPolygon = new array<vector>;
+		for (int i = 0; i < resolution; i++) {
+			float theta = i * (2 * Math.PI / resolution);
+			float pointPosX = (int) (center[0] + (radius * Math.Cos(theta)));
+			float pointPosY = (int) (center[2] + (radius * Math.Sin(theta)));
+			vector point = {pointPosX, 0, pointPosY};
+			circularPolygon.Insert(point);
+		}
+		
+		return circularPolygon;
+	}
+	
+	
+	
+	// CIRCULAR LINE
+	void renderLineCircled(vector center, float radius, int color, float width, int resolution = 36) {
+		array<vector> points = generateCircularPolygon(center, radius, resolution);
+		array<vector> translatedPoints = translatePoints(points);
+		renderPolygonToLines(translatedPoints, color, width);
+	}
+	void renderLineCircledOutlined(vector center, float radius, int color, float width, int outlineColor, float outlineWidth, int resolution = 36) {
+		array<vector> points = generateCircularPolygon(center, radius, resolution);
+		array<vector> translatedPoints = translatePoints(points);
+		renderPolygonToLinesOutlined(translatedPoints, color, width, outlineColor, outlineWidth);
+	}
+	
+	
+	
+	// RECTANGULAR LINE 
+	void renderLineRectangled(vector start, vector end, int color, float width) {
+		array<vector> points = generateRectangularPolygon(start, end);
+		array<vector> translatedPoints = translatePoints(points);
+		renderPolygonToLines(translatedPoints, color, width);
+	}
+	void renderLineRectangledOutlined(vector start, vector end, int color, float width, int outlineColor,  float outlineWidth) {
+		array<vector> points = generateRectangularPolygon(start, end);
+		array<vector> translatedPoints = translatePoints(points);
+		//renderPolygon(translatedPoints, color);
+		renderPolygonToLinesOutlined(translatedPoints, color, width, outlineColor, outlineWidth);
+	}
+	
+
+	
+	// TRANSLATE MAP-COORDINATE TO TRANSLATED-SCREEN-COORDINATE
+	array<vector> translatePoints(array<vector> points) {
+		array<vector> translatedPoints = new array<vector>;
+
+		foreach (int currentIndex, vector currentPoint : points) {
+			int translatedPosX;
+			int translatedPosY;
+			m_MapEntity.WorldToScreen(currentPoint[0], currentPoint[2], translatedPosX, translatedPosY, true);
+			vector translatedPoint = {translatedPosX, 0, translatedPosY};
+			translatedPoints.Insert(translatedPoint);
+		}
+		
+		return translatedPoints;
+	}	
+	
+	
+	
+	// RENDER GENERIC POLYGON
+	void renderPolygon(array<vector> translatedPoints, int color) {
 		PolygonDrawCommand drawCommand = new PolygonDrawCommand();		
 		drawCommand.m_iColor = color;
 		drawCommand.m_Vertices = new array<float>;
 		
-		array<vector> points = new array<vector>;
-		
-		int mapPosX;
-		int mapPosY;
-		m_MapEntity.WorldToScreen(center[0], center[2], mapPosX, mapPosY, true);
-		
-		float scaledRadius = radius * m_MapEntity.GetCurrentZoom();
-		for (int i = 0; i < resolution; i++) {
-			float theta = i * (2 * Math.PI / resolution);
-			float screenPointPosX = (int) (mapPosX + scaledRadius * Math.Cos(theta));
-			float screenPointPosY = (int) (mapPosY + scaledRadius * Math.Sin(theta));
-			vector point = {screenPointPosX, 0, screenPointPosY};
-			points.Insert(point);
-		}
-		
-		foreach (int currentIndex, vector currentPoint : points) {
+		foreach (int currentIndex, vector currentPoint : translatedPoints) {
 			drawCommand.m_Vertices.Insert(currentPoint[0]);
 			drawCommand.m_Vertices.Insert(currentPoint[2]);
 		}
 		renderCommands.Insert(drawCommand);
-		
-		return points;
 	}	
+
 	
-	void renderCircleOutlined(vector center, float radius, int color, int outlineColor,  float outlineWidth, int resolution = 36) {
-		array<vector> points = renderCircle(center, radius, color, resolution);
-			
+	
+	// RENDER GENERIC POLYGON -> LINE
+		void renderPolygonToLines(array<vector> translatedPoints, int color,  float width) {
 		vector firstPoint;
 		vector lastPoint;
-		foreach (int currentIndex, vector currentPoint : points) {
+		foreach (int currentIndex, vector currentPoint : translatedPoints) {
 			if (!lastPoint) {
 				firstPoint = currentPoint;
 				lastPoint = currentPoint;
 			} else {
-				renderRawLine(lastPoint, currentPoint, outlineColor, outlineWidth);
+				renderRawLine(lastPoint, currentPoint, color, width);
 				lastPoint = currentPoint;
 			}
 		}
 		
-		// close renderLine
-		renderLine(firstPoint, lastPoint, outlineColor, outlineWidth);
-	}	
-	
-	void renderLine(vector start, vector end, int color, float width) {
-		renderLineOutlined(start, end, color, width, 0, 0.0);
+		// close line
+		renderRawLine(firstPoint, lastPoint, color, width);
 	}
-	void renderLineOutlined(vector start, vector end, int color, float width, int outlineColor,  float outlineWidth) {
-		LineDrawCommand drawCommand = new LineDrawCommand();		
-		drawCommand.m_iColor = color;
-		drawCommand.m_fWidth = width;
-		drawCommand.m_iOutlineColor = outlineColor;
-		drawCommand.m_fOutlineWidth = outlineWidth;
-		drawCommand.m_Vertices = new array<float>;
+	void renderPolygonToLinesOutlined(array<vector> translatedPoints, int color,  float width, int colorOutline,  float widthOutline) {
+		vector firstPoint;
+		vector lastPoint;
+		foreach (int currentIndex, vector currentPoint : translatedPoints) {
+			if (!lastPoint) {
+				firstPoint = currentPoint;
+				lastPoint = currentPoint;
+			} else {
+				renderRawLineOutlined(lastPoint, currentPoint, color, width, colorOutline, widthOutline);
+				lastPoint = currentPoint;
+			}
+		}
 		
-		int mapPosStartX;
-		int mapPosStartY;
-		m_MapEntity.WorldToScreen(start[0], start[2], mapPosStartX, mapPosStartY, true);
-		
-		int mapPosEndX;
-		int mapPosEndY;
-		m_MapEntity.WorldToScreen(end[0], end[2], mapPosEndX, mapPosEndY, true);
-
-		drawCommand.m_Vertices.Insert(mapPosStartX);
-		drawCommand.m_Vertices.Insert(mapPosStartY);
-		drawCommand.m_Vertices.Insert(mapPosEndX);
-		drawCommand.m_Vertices.Insert(mapPosEndY);
-
-		renderCommands.Insert(drawCommand);
+		// close line
+		renderRawLineOutlined(firstPoint, lastPoint, color, width, colorOutline, widthOutline);
 	}
 	
+	
+	
+	// RAW LINES WITH TRANSLATED COORDINATES
 	private void renderRawLine(vector start, vector end, int color, float width) {
 		renderRawLineOutlined(start, end, color, width, 0, 0.0);
 	}
@@ -146,63 +250,5 @@ class RefCom_MapModuleRenderer  : SCR_MapModuleBase {
 
 		renderCommands.Insert(drawCommand);
 	}
-	
-	void renderRectangle(vector start, vector end, int color) {
-		PolygonDrawCommand drawCommand = new PolygonDrawCommand();		
-		drawCommand.m_iColor = color;
-		drawCommand.m_Vertices = new array<float>;
-		
-		int mapPosStartCornerX;
-		int mapPosStartCornerY;
-		m_MapEntity.WorldToScreen(start[0], start[2], mapPosStartCornerX, mapPosStartCornerY, true);
-		
-		int mapPosEndCornerX;
-		int mapPosEndCornerY;
-		m_MapEntity.WorldToScreen(end[0], end[2], mapPosEndCornerX, mapPosEndCornerY, true);
-
-		drawCommand.m_Vertices.Insert(mapPosStartCornerX);
-		drawCommand.m_Vertices.Insert(mapPosStartCornerY);
-		
-		drawCommand.m_Vertices.Insert(mapPosEndCornerX);
-		drawCommand.m_Vertices.Insert(mapPosStartCornerY);
-		
-		drawCommand.m_Vertices.Insert(mapPosEndCornerX);
-		drawCommand.m_Vertices.Insert(mapPosEndCornerY);
-		
-		drawCommand.m_Vertices.Insert(mapPosStartCornerX);
-		drawCommand.m_Vertices.Insert(mapPosEndCornerY);
-
-		renderCommands.Insert(drawCommand);
-	}
-	void renderRectangleOutlined(vector start, vector end, int color, int outlineColor,  float outlineWidth) {
-		renderRectangle(start, end, color);
-		
-		vector end1 = {end[0],0,start[2]};
-		renderLine(start, end1, outlineColor, outlineWidth);
-		
-		vector start2 = end1;
-		renderLine(start2, end, outlineColor, outlineWidth);
-		
-		vector end3 = {start[0],0,end[2]};
-		renderLine(end, end3, outlineColor, outlineWidth);
-		
-		vector start4 = end3;
-		renderLine(start4, start, outlineColor, outlineWidth);
-	}
-
-	/*
-	// does not work now; maybe due to missing drawCommand.m_iFontPropertiesId.
-	// spec for this is missing :-(
-	void renderText(vector start, string text, int color, float size, float rotation = 0) {
-		TextDrawCommand drawCommand = new TextDrawCommand();
-		drawCommand.m_iColor = color;
-		drawCommand.m_Position = start;
-		drawCommand.m_sText = text;
-		drawCommand.m_fSize = size;
-		drawCommand.m_fRotation = rotation;
-		
-		renderCommands.Insert(drawCommand);
-	}
-	*/
 
 };
