2786f82bb662299ccbe757a2a15dc3321901ff9c
Sven Carrillo Castillo <carrillo.castillo.sven@googlemail.com>
2023-06-09 17:03:27 +0200

    refactoring to generic polygon and line render methods

--- scripts/Game/RefCom/MapRenderer/Modules/RefCom_MapModuleRenderer.c
+++ scripts/Game/RefCom/MapRenderer/Modules/RefCom_MapModuleRenderer.c
@@ -35,13 +35,15 @@ class RefCom_MapModuleRenderer  : SCR_MapModuleBase {
 		renderCircle("1100 0 1100", 100, ARGB(255, 0, 255, 0) );
 		renderCircle("1200 0 1200", 75, ARGB(255, 0, 0, 255) );
 		
+		renderCircleOutlined("2000 0 2000", 400, ARGB(255, 0, 0, 255), ARGB(255, 0, 255, 0), 4);
+		
 		renderLine("800 0 800", "1500 0 1500", ARGB(255, 0, 0, 0), 2);
 		renderLineOutlined("1200 0 800", "1900 0 1500", ARGB(255, 0, 255, 0), 10, ARGB(255, 0, 0, 0), 2);
 		
 		//renderRectangle("2000 0 1000", "3000 0 2000", ARGB(255, 0, 0, 255));
 		renderRectangleOutlined("3150 0 1150", "3850 0 1850", ARGB(255, 0, 0, 255),  ARGB(255, 255, 0, 0), 2);
 		
-		renderText("800 0 1000", "Das ist mein Text", ARGB(255, 0, 0, 0), 50);
+		//renderText("800 0 1000", "Das ist mein Text", ARGB(255, 0, 0, 0), 50);
 	}
 	
 	override void Update(float timeSlice) {	
@@ -51,26 +53,52 @@ class RefCom_MapModuleRenderer  : SCR_MapModuleBase {
 		Render()
 	}
 	
-	void renderCircle(vector center, float radius, int color, int resolution = 36) {
+	array<vector> renderCircle(vector center, float radius, int color, int resolution = 36) {
 		PolygonDrawCommand drawCommand = new PolygonDrawCommand();		
 		drawCommand.m_iColor = color;
 		drawCommand.m_Vertices = new array<float>;
+		
+		array<vector> points = new array<vector>;
 		
 		int mapPosX;
 		int mapPosY;
 		m_MapEntity.WorldToScreen(center[0], center[2], mapPosX, mapPosY, true);
 		
 		float scaledRadius = radius * m_MapEntity.GetCurrentZoom();
-		
 		for (int i = 0; i < resolution; i++) {
 			float theta = i * (2 * Math.PI / resolution);
 			float screenPointPosX = (int) (mapPosX + scaledRadius * Math.Cos(theta));
 			float screenPointPosY = (int) (mapPosY + scaledRadius * Math.Sin(theta));
-			drawCommand.m_Vertices.Insert(screenPointPosX);
-			drawCommand.m_Vertices.Insert(screenPointPosY);
+			vector point = {screenPointPosX, 0, screenPointPosY};
+			points.Insert(point);
 		}
 		
+		foreach (int currentIndex, vector currentPoint : points) {
+			drawCommand.m_Vertices.Insert(currentPoint[0]);
+			drawCommand.m_Vertices.Insert(currentPoint[2]);
+		}
 		renderCommands.Insert(drawCommand);
+		
+		return points;
+	}	
+	
+	void renderCircleOutlined(vector center, float radius, int color, int outlineColor,  float outlineWidth, int resolution = 36) {
+		array<vector> points = renderCircle(center, radius, color, resolution);
+			
+		vector firstPoint;
+		vector lastPoint;
+		foreach (int currentIndex, vector currentPoint : points) {
+			if (!lastPoint) {
+				firstPoint = currentPoint;
+				lastPoint = currentPoint;
+			} else {
+				renderRawLine(lastPoint, currentPoint, outlineColor, outlineWidth);
+				lastPoint = currentPoint;
+			}
+		}
+		
+		// close renderLine
+		renderLine(firstPoint, lastPoint, outlineColor, outlineWidth);
 	}	
 	
 	void renderLine(vector start, vector end, int color, float width) {
@@ -96,6 +124,25 @@ class RefCom_MapModuleRenderer  : SCR_MapModuleBase {
 		drawCommand.m_Vertices.Insert(mapPosStartY);
 		drawCommand.m_Vertices.Insert(mapPosEndX);
 		drawCommand.m_Vertices.Insert(mapPosEndY);
+
+		renderCommands.Insert(drawCommand);
+	}
+	
+	private void renderRawLine(vector start, vector end, int color, float width) {
+		renderRawLineOutlined(start, end, color, width, 0, 0.0);
+	}
+	private void renderRawLineOutlined(vector start, vector end, int color, float width, int outlineColor,  float outlineWidth) {
+		LineDrawCommand drawCommand = new LineDrawCommand();		
+		drawCommand.m_iColor = color;
+		drawCommand.m_fWidth = width;
+		drawCommand.m_iOutlineColor = outlineColor;
+		drawCommand.m_fOutlineWidth = outlineWidth;
+		drawCommand.m_Vertices = new array<float>;
+		
+		drawCommand.m_Vertices.Insert(start[0]);
+		drawCommand.m_Vertices.Insert(start[2]);
+		drawCommand.m_Vertices.Insert(end[0]);
+		drawCommand.m_Vertices.Insert(end[2]);
 
 		renderCommands.Insert(drawCommand);
 	}
