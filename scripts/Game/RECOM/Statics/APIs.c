56a009e1910f536c827b5de925a9d68f417b9514
Sven Carrillo Castillo <carrillo.castillo.sven@googlemail.com>
2023-08-09 21:54:33 +0200

    change url /api/v1/map/clusters -> /api/v1/map/clusters/form

--- scripts/Game/RECOM/Statics/APIs.c
+++ scripts/Game/RECOM/Statics/APIs.c
@@ -6,12 +6,12 @@ class APIs {
 	static string GET_HEALTH = "/health";
 	
 	// /map
-	static string GET_MAP_META_MAPS = "/api/v1/map/meta/maps";
+    // static string GET_MAP_META_MAPS = "/api/v1/map/meta/maps";
 	
 	static string POST_MAP_SCANNER_TRANSACTION_OPEN = "/api/v1/map/scanner/transaction/open";
 	static string POST_MAP_SCANNER_TRANSACTION_ENTITIES = "/api/v1/map/scanner/transaction/entities";
 	static string POST_MAP_SCANNER_TRANSACTION_COMMIT = "/api/v1/map/scanner/transaction/commit";
 	
-	static string POST_MAP_CLUSTERS = "/api/v1/map/clusters";
+	static string POST_MAP_CLUSTERS = "/api/v1/map/clusters/form";
 		
 }
