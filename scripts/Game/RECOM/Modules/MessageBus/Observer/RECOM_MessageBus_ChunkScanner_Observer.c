6f9bb7ec81280643e5df325be1368414563ff780
Sven Carrillo Castillo <carrillo.castillo.sven@googlemail.com>
2024-03-24 13:43:20 +0100

    improve chunk request logging

--- scripts/Game/RECOM/Modules/MessageBus/Observer/RECOM_MessageBus_ChunkScanner_Observer.c
+++ scripts/Game/RECOM/Modules/MessageBus/Observer/RECOM_MessageBus_ChunkScanner_Observer.c
@@ -11,6 +11,8 @@ class RECOM_MessageBus_ChunkScanner_Observer : RECOM_MessageBus_Observer {
 				RECOM_MessageBus_MapTopographyChunkScanRequestDto chunkRequest = new RECOM_MessageBus_MapTopographyChunkScanRequestDto();
 				chunkRequest.ExpandFromRAW(message.payload);
 				
+				SLF4R.normal(string.Format("%1: Request Chunk generation for %2 : x%3,y%4", ClassName(), chunkRequest.mapName, chunkRequest.chunkCoordinateX, chunkRequest.chunkCoordinateY));
+				
 				ref RECOM_MapTopographyChunkScanner scanner = new RECOM_MapTopographyChunkScanner(chunkRequest.chunkCoordinateX, chunkRequest.chunkCoordinateY);
 				scanners.Insert(scanner);
 				
